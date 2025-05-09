dad39832d66c627500e3b769ce540ad85e57b7e9
Leonardo Raposo <82468796+Kuhaiku@users.noreply.github.com>
2025-05-09 17:19:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,13 @@ const path = require('path');
 const app = express();
 const PORT = 3030;
 
-// Configurar a pasta de arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Redirecionar a rota inicial para o form.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'form.html'));
 });
 
-// Iniciar o servidor
-app.listen(PORT, 'localhost', () => {
+// Escutando em todas as interfaces (recomendado para Docker)
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
-
